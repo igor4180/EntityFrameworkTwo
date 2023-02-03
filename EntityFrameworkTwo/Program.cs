6beb3ec2ccc212b996f94b6a208a8315557d77a4
--- <---.mainry>
2023-02-03 20:09:56 +0300

    Добавьте файлы проекта.

---
+++ EntityFrameworkTwo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkTwo
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//AddCategory(new Category(2, "Холодильники"));
+			GetAllCategory();
+
+			Console.ReadLine();
+		}
+
+		static void AddCategory(Category item) 
+		{
+			using (StoreEntities storeEntities = new StoreEntities())
+			{
+				storeEntities.Category.Add(item);
+				storeEntities.SaveChanges();
+			}
+		}
+		static void GetAllCategory()
+		{
+			using (StoreEntities storeEntities = new StoreEntities())
+			{
+				List<Category> list = storeEntities.Category.ToList();
+				foreach (Category item in list)
+				{ 
+					Console.WriteLine(item);
+				}
+			}
+		}
+	}
+}
